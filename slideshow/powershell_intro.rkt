20b9c9041725c6bbc79bafd77527a74abaf3735b
Kai Han <KHan6@slb.com>
2015-11-13 09:19:45 +0800

    wording modification.

--- slideshow/powershell_intro.rkt
+++ slideshow/powershell_intro.rkt
@@ -18,15 +18,15 @@
 (define (s-highlight str size) (colorize (text str '(bold . modern) size) "red"))
 (define (s-keyword str size) (colorize (text str '(bold . modern) size) "blue"))
 
-(define (goto-lt-then-show-topic title timeout size x-offset y-offset being-smaller final-display hooker)  
-  (slide #:timeout timeout 
+(define (goto-lt-then-show-topic title timeout size x-offset y-offset being-smaller final-display hooker)
+  (slide #:timeout timeout
          #:title title
          #:layout 'top
-     (pin-over hooker x-offset y-offset 
+     (pin-over hooker x-offset y-offset
          (being-smaller size))
-      ((if (and (equal? y-offset 0) 
-                (equal? x-offset 0) 
-                (equal? timeout #f)) 
+      ((if (and (equal? y-offset 0)
+                (equal? x-offset 0)
+                (equal? timeout #f))
                      ident ghost) final-display)))
 
 ;
@@ -49,28 +49,17 @@
 (define highlight-power
   (text "Power" `(bold . ,(current-main-font)) 48))
 
-(slide (hc-append 
+(slide (hc-append
          (ghost shell_examples)
          (ht-append (ghost highlight-power) (s-t "Shell" 48))))
 
-(slide (hc-append 
+(slide (hc-append
          shell_examples
          (ht-append (ghost highlight-power) (s-t "Shell" 48))))
 
-(slide (hc-append 
+(slide (hc-append
          (ghost shell_examples)
          (ht-append highlight-power (s-t "Shell" 48))))
-
-;
-; Slide for what PowerShell can do
-;
-(slide #:title "What can PowerShell do?"
-   (table 2 
-          (list (t "first thing") (t "2") (t "second thing") (t "3"))
-          (list* lc-superimpose cc-superimpose)
-          cc-superimpose
-          gap-size
-          gap-size))
 
 ;
 ; Slide for simplest PowerShell
@@ -81,21 +70,21 @@
    'next
    (item #:bullet (blank) (s-t "> ls" font-size-in-powershell-101))
    'next
-   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101) 
+   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101)
                              (text "Get-ChildItem" '(bold . modern) font-size-in-powershell-101)))
    'next
-   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101) 
+   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101)
                              (text "Stop-Serivce " '(bold . modern) font-size-in-powershell-101)
-                             (s-t "IntegrationWorkerDaemon" font-size-in-powershell-101)))
+                             (s-t "Jenkins" font-size-in-powershell-101)))
    'next
-   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101) 
+   (item #:bullet (blank) (h (s-t "> " font-size-in-powershell-101)
                              (text "New-WebApplication " '(bold . modern) font-size-in-powershell-101)
                              (s-t "-name \"Default Web Site\"" font-size-in-powershell-101)))
-   
+
    'next
    (blank)
    (colorize (h (text "All" '(bold . modern) (current-font-size)) (t " commands are in ") (text "\"COMMAND + ARGS\"" '(italic bold . modern) (current-font-size)) (t " style.")) "blue")
-)     
+)
 
 ;
 ; Slide for extensions of PowerShell
@@ -104,7 +93,7 @@
 
 (slide (s-t "Write your own Functions" 50))
 
-; 
+;
 ; Slide for PowerShell Function
 ;
 (define func-keyword (keyword "Function "))
@@ -112,7 +101,7 @@
 (define arg-list (ps "($name) "))
 (define func-body (ps "    Write-Host Hello $name"))
 
-(define source-snap 
+(define source-snap
    (inset (vl-append (* gap-size 1.5) (para func-keyword func-name arg-list (t " {"))
                   (para func-body)
                   (para (t "}"))) 200 0 0 0))
@@ -136,13 +125,13 @@
                                (ghost w/arg-list)
                                func-body
                                cb-find)])
-   
-   (cc-superimpose sence 
+
+   (cc-superimpose sence
                    (show-func-keyword w/func-keyword)
                    (show-func-name w/func-name)
                    (show-arg-list w/arg-list)
                    (show-func-body w/func-body)))))
-  
+
 (highlight-function-example ghost ghost ghost ghost)
 (highlight-function-example ident ghost ghost ghost)
 (highlight-function-example ident ident ghost ghost)
@@ -151,7 +140,7 @@
 ;
 ; Slide for PowerShell invocation
 ;
-(define (function-invocations show-group-1 show-group-2 show-group-3) 
+(define (function-invocations show-group-1 show-group-2 show-group-3)
   (slide #:title "Function Invocation"
     (v (* gap-size 1.5)
        (show-group-1 (ps "> Hello (\"World\")"))
@@ -160,12 +149,12 @@
        (show-group-3 (ps "> Hello -name World"))
        (show-group-3 (ps "> Hello -name \"World\"")))
     (blank)
-    ((if (and (equal? show-group-1 ident) 
-              (equal? show-group-2 ident) 
+    ((if (and (equal? show-group-1 ident)
+              (equal? show-group-2 ident)
               (equal? show-group-3 ident)) ident ghost)
-         (colorize (h (text "All" '(bold . modern) (current-font-size)) 
-                      (t " commands are in ") 
-                      (text "\"COMMAND + ARGS\"" '(italic bold . modern) (current-font-size)) (t " style.")) 
+         (colorize (h (text "All" '(bold . modern) (current-font-size))
+                      (t " commands are in ")
+                      (text "\"COMMAND + ARGS\"" '(italic bold . modern) (current-font-size)) (t " style."))
                    "blue"))))
 
 (function-invocations ident ghost ghost)
@@ -180,7 +169,7 @@
 (define smallest-code-block (ghost (blank client-w (/ client-h 5))))
 
 (define (func-if-fielexist size)
-  (v (* gap-size (/ size (current-font-size))) 
+  (v (* gap-size (/ size (current-font-size)))
             (h (s-ps "Function If-FileExist($filePath, " size) (s-highlight "$action" size) (s-ps ") {" size))
             (s-ps "    if (Test-Path $filePath) {" size)
             (h (s-ps "        " size) (s-highlight "& $action($filePath)" size))
@@ -188,7 +177,7 @@
             (s-ps "}" size)
           ))
 
-(define script-block-invocation 
+(define script-block-invocation
   (v gap-size
     (ps "$output=\"bin\\Debug\"")
     (h (ps "If-FileExist -filePath $output -action") (highlight " {"))
@@ -202,39 +191,39 @@
        [title "Scrpit Block (aka. Closure)"])
   (goto-lt-then-show-topic title #f (current-font-size) init-x-offset init-y-offset func-if-fielexist script-block-invocation smallest-code-block)
   (for ([i (in-range steps)])
-    (goto-lt-then-show-topic 
+    (goto-lt-then-show-topic
                         title
-                        0.05 
-                        (- (current-font-size) i ) 
+                        0.05
+                        (- (current-font-size) i )
                         (- init-x-offset (* i (/ init-x-offset steps)))
                         (- init-y-offset (* i (/ init-y-offset steps)))
                         func-if-fielexist
                         script-block-invocation
                         smallest-code-block))
-  (goto-lt-then-show-topic 
+  (goto-lt-then-show-topic
                         title
-                        #f 
-                        (- (current-font-size) steps) 
+                        #f
+                        (- (current-font-size) steps)
                         0
                         0
                         func-if-fielexist
                         script-block-invocation
-                        smallest-code-block)) 
+                        smallest-code-block))
 
 ;
 ; Slides for implements "Task"
 ;
 (slide #:title "Define a Task"
    (v (* gap-size 2)
-       (frame 
+       (frame
         (inset
         (v
         (ps "<Target Name=\"Compile\" -depends Clean>")
         (ps "    <Exec Cmd=\"$Yam build YOUR_PROJ\">")
         (ps "</Target>")
-        ) 20)) 
-      (frame  
-       (inset (v 
+        ) 20))
+      (frame
+       (inset (v
         (ps "task :compile do ")
         (ps "    sh \"gcc *.c -o proj.o\"")
         (ps "end")
@@ -249,7 +238,7 @@
       (let* ([task-invocation-font-size 28]
              [func-name (s-ps "Task " task-invocation-font-size)]
              [first-arg (h (s-ps "-name " task-invocation-font-size) (s-highlight "Package" task-invocation-font-size))]
-             [second-arg (h (s-ps " -depends " task-invocation-font-size) (s-highlight "UnitTest, FunctionTest " task-invocation-font-size))]
+             [second-arg (h (s-ps " -depends " task-invocation-font-size) (s-highlight "UnitTest" task-invocation-font-size))]
              [third-arg (s-ps "    # Package ..." task-invocation-font-size)]
              [sence (v (* gap-size 1.2)
                        (h func-name first-arg second-arg (ps " {"))
@@ -271,8 +260,8 @@
                                     (ghost w/second-arg)
                                     third-arg
                                     cb-find)]
-             
-             ) 
+
+             )
         (cc-superimpose sence
                         (show-func-name w/func-name)
                         (show-first-arg w/first-arg)
@@ -290,7 +279,7 @@
 
 (slide #:title "What's different between If-FileExist"
        (item "Some args are optional")
-       (subitem "depends")       
+       (subitem "depends")
        (subitem "script block")
        (item "Sequence is not fixed"))
 
@@ -312,9 +301,9 @@
   (v
    (s-ps "Function Task {" font-size)
    (s-ps "  param(" font-size)
-   (h (s-highlight "    [Parameter(Mandatory=1)]" font-size) (s-ps " $name=$null," font-size))
-   (h (s-highlight "    [Parameter(Mandatory=0)]" font-size)(s-ps " $action=$null," font-size))
-   (h (s-highlight "    [Parameter(Mandatory=0)]" font-size) (s-ps " $depends=@()" font-size))
+   (h (s-highlight "    [Parameter(Mandatory=$true)]" font-size) (s-ps " $name=$null," font-size))
+   (h (s-highlight "    [Parameter(Mandatory=$false)]" font-size)(s-ps " $action=$null," font-size))
+   (h (s-highlight "    [Parameter(Mandatory=$false)]" font-size) (s-ps " $depends=@()" font-size))
    (s-ps "  )" font-size)
    (s-ps "  # Invoke task ..." font-size)
    (s-ps "}" font-size))))
@@ -324,9 +313,9 @@
   (v
    (s-ps "Function Task {" font-size)
    (s-ps "  param(" font-size)
-   (h (s-ps "    [Parameter(Mandatory=1)]" font-size) (s-highlight "[string]" font-size) (s-ps " $name=$null," font-size))
-   (h (s-ps "    [Parameter(Mandatory=0)]" font-size) (s-highlight "[scriptblock]" font-size) (s-ps " $action=$null," font-size))
-   (h (s-ps "    [Parameter(Mandatory=0)]" font-size) (s-highlight "[string[]]" font-size) (s-ps " $depends=@()" font-size))
+   (h (s-ps "    [Parameter(Mandatory=$true)]" font-size) (s-highlight "[string]" font-size) (s-ps " $name=$null," font-size))
+   (h (s-ps "    [Parameter(Mandatory=$false)]" font-size) (s-highlight "[scriptblock]" font-size) (s-ps " $action=$null," font-size))
+   (h (s-ps "    [Parameter(Mandatory=$false)]" font-size) (s-highlight "[string[]]" font-size) (s-ps " $depends=@()" font-size))
    (s-ps "  )" font-size)
    (s-ps "  # Invoke task ..." font-size)
    (s-ps "}" font-size))))
@@ -338,22 +327,22 @@
 ;
 (define hooker-for-language-features (ghost (blank client-w (/ client-h 10))))
 
-(define (show-powershell-language-feature feature feature-example) 
+(define (show-powershell-language-feature feature feature-example)
   (let* ([init-x-offset 350]
        [init-y-offset 300]
        [steps 10]
        [title-font-size 50])
   (goto-lt-then-show-topic #f #f title-font-size init-x-offset init-y-offset feature feature-example hooker-for-language-features)
   (for ([i (in-range steps)])
-    (goto-lt-then-show-topic #f 0.05 
-                        (- title-font-size i ) 
+    (goto-lt-then-show-topic #f 0.05
+                        (- title-font-size i )
                         (- init-x-offset (* i (/ init-x-offset steps)))
                         (- init-y-offset (* i (/ init-y-offset steps)))
                         feature
                         feature-example
                         hooker-for-language-features))
   (goto-lt-then-show-topic #f #f
-                        (- title-font-size steps) 
+                        (- title-font-size steps)
                         0
                         0
                         feature
@@ -365,7 +354,7 @@
 
 (define (dynamical-type size) (s-t "Dynamical Typed" size))
 
-(define dynamical-type-example 
+(define dynamical-type-example
   (v (* gap-size 1.4)
     (h (ps "> ") (highlight "$tmp ") (ps "= 1"))
     (h (ps "> ") (keyword "Write-Host ") (highlight "$tmp"))
