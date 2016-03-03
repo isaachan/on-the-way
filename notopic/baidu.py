56faa7d4dc8a92e69d0c402e97eb8e755f4b6c84
Han Kai <isaachanstar@gmail.com>
2016-03-03 18:35:20 +0800

    refactor.

--- notopic/baidu.py
+++ notopic/baidu.py
@@ -19,20 +19,20 @@ class AudioToTextRequest:
 		self.lan = "zh"
 
 	def get_speech(self, voiceFilePath):	
-
 		fh = open(voiceFilePath, 'r')
 		return base64.b64encode(fh.read())
-	
 
 	def get_accesstoken(self):
 		url = 'https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=j43LMaLBD30gQ5GTtOGjzigL&client_secret=fa8525a54a70971e06a97276cc409491'
 		response = requests.get(url).text
 		return json.loads(response)['access_token']
-		
 
-req = AudioToTextRequest('voice.wav')
+	def get_postdata(self):
+		return json.dumps(self, default=lambda o: o.__dict__)
 
-data = json.dumps(req, default=lambda o: o.__dict__)
+data = AudioToTextRequest('voice.wav').get_postdata()
+
+#data = json.dumps(req, default=lambda o: o.__dict__)
 
 api_url = "http://vop.baidu.com/server_api"
 
