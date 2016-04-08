5e8065491c28373e901c13b6462eed5c18d752d3
Han Kai <isaachanstar@gmail.com>
2016-04-08 19:19:06 +0800

    update picture

--- slideshow/machine-learning/src/movie-scatter.py
+++ slideshow/machine-learning/src/movie-scatter.py
@@ -8,7 +8,12 @@ movies = [[3,   104, 'love'],   \
           [1,   81,  'love'],   \
           [101, 10,  'action'], \
           [99,  5,   'action'], \
-          [98,  2,   'action']]
+          [98,  2,   'action'], \
+          [18,  90,  '??']]
+
+typeColorMap = { 'love'  : 'red',
+                 'action': 'blue',
+                 '??'    : 'gray'}
 
 dataset = []
 sizes   = []
@@ -20,13 +25,12 @@ ax = fig.add_subplot(111)
 for movie in movies:
     dataset.append(movie[0:-1])
     type = movie[-1:][0]
-    sizes.append(60)
-    colors.append('red' if (type == 'love') else 'blue')
+    sizes.append(120 if (type == '??') else 60)
+    colors.append(typeColorMap[type])
     ax.annotate(movie[2], (movie[0]+2, movie[1]-1))
     
 dataset = array(dataset)
 ax.scatter(dataset[:,0], dataset[:,1], array(sizes), array(colors))
-
 
 if len(sys.argv) == 2 and sys.argv[1] == '-save':
     plt.savefig("images/movie-scatter.png")
