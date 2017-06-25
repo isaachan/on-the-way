e4dab98caf82a4091aa43e1e027c7ebe89f0075d
Han Kai <isaachanstar@gmail.com>
2017-06-25 12:22:55 +0800

    add alert producer.

---
+++ rabbitmq/notification/alert_producer.py
@@ -0,0 +1,30 @@
+import json, pika
+from optparse import OptionParser
+
+opt_parser = OptionParser()
+opt_parser.add_option("-r",
+                      "--routing-key",
+                      dest = "routing_key",
+                      help = "Routing key for message (e.g. myalert.im)")
+opt_parser.add_option("-m",
+                      "--message",
+                      dest = "message",
+                      help = "Message text for alert")
+
+args = opt_parser.parse_args()[0]
+creds_broker = pika.PlainCredentials("alert_user", "alertme")
+conn_params = pika.ConnectionParameters("localhost",
+                                        virtual_host = "/*",
+                                        credentials = creds_broker)
+
+conn_broker = pika.BlockingConnection(conn_params)
+channel = conn_broker.channel()
+
+msg = json.dumps(args.message)
+msg_props = pika.BasicProperties()
+msg_props.content_type = "application/json"
+msg_props.durable = False
+
+channel.basic_publish(body = msg, exchange = "alters", properties = msg_props, routing_key = args.routing_key)
+
+print("send message %s tagged with routing key '%s' to exchange '/'." % (json.dumps(args.message), args.routing_key))
