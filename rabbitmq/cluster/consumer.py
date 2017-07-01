477c3988c5f7bdc07b17866b80958f50400e4ef1
Han Kai <isaachanstar@gmail.com>
2017-07-01 12:39:21 +0800

    cluster and HA queue.

--- rabbitmq/cluster/consumer.py
+++ rabbitmq/cluster/consumer.py
@@ -7,6 +7,16 @@ conn_params = pika.ConnectionParameters("localhost",
 conn_broker = pika.BlockingConnection(conn_params)
 channel = conn_broker.channel()
 
-queue_args = {"x-ha-proxy": "all"}
-channel.queue_declare(queue = "cluster_queue", arguments = queue_args)
+'''
+The use of queue arguments to declare a queue as HA dates from RabbitMQ 
+2.x. In 3.x we changed this to use policies as you've already seen - 
+since applications should not have to know about whether their queues 
+are HA.
+
+So use rabbitmqctl set_policy (or the management plugin).
+ 
+> rabbitmqctl set_policy ha-fed "cluster_queue" '{"ha-mode":"all"}' --priority 1 --apply-to queues
+'''
+
+channel.queue_declare(queue = "cluster_queue")
 
--- rabbitmq/cluster/setup.sh
+++ rabbitmq/cluster/setup.sh
@@ -1,28 +1,28 @@
 RABBITMQ_NODE_PORT=5672 RABBITMQ_NODENAME=rabbit rabbitmq-server -detached
 
-RABBITMQ_NODE_PORT=5801 RABBITMQ_NODENAME=rabbit_1 rabbitmq-server -detached
-rabbitmqctl -n rabbit_1@`hostname` stop_app
-rabbitmqctl -n rabbit_1@`hostname` reset
-rabbitmqctl -n rabbit_1@`hostname` join_cluster rabbit_1@`hostname`
-rabbitmqctl -n rabbit_1@`hostname` start_app
+RABBITMQ_NODE_PORT=5801 RABBITMQ_NODENAME=rabbit_a rabbitmq-server -detached
+rabbitmqctl -n rabbit_a@`hostname` stop_app
+rabbitmqctl -n rabbit_a@`hostname` reset
+rabbitmqctl -n rabbit_a@`hostname` join_cluster rabbit@`hostname`
+rabbitmqctl -n rabbit_a@`hostname` start_app
  
-RABBITMQ_NODE_PORT=5802 RABBITMQ_NODENAME=rabbit_2 rabbitmq-server -detached
-rabbitmqctl -n rabbit_2@`hostname` stop_app
-rabbitmqctl -n rabbit_2@`hostname` reset
-rabbitmqctl -n rabbit_2@`hostname` join_cluster rabbit_1@`hostname`
-rabbitmqctl -n rabbit_2@`hostname` start_app
+RABBITMQ_NODE_PORT=5802 RABBITMQ_NODENAME=rabbit_b rabbitmq-server -detached
+rabbitmqctl -n rabbit_b@`hostname` stop_app
+rabbitmqctl -n rabbit_b@`hostname` reset
+rabbitmqctl -n rabbit_b@`hostname` join_cluster rabbit@`hostname`
+rabbitmqctl -n rabbit_b@`hostname` start_app
 
-RABBITMQ_NODE_PORT=5803 RABBITMQ_NODENAME=rabbit_3 rabbitmq-server -detached
-rabbitmqctl -n rabbit_3@`hostname` stop_app
-rabbitmqctl -n rabbit_3@`hostname` reset
-rabbitmqctl -n rabbit_3@`hostname` join_cluster rabbit_1@`hostname`
-rabbitmqctl -n rabbit_3@`hostname` start_app
+RABBITMQ_NODE_PORT=5803 RABBITMQ_NODENAME=rabbit_c rabbitmq-server -detached
+rabbitmqctl -n rabbit_c@`hostname` stop_app
+rabbitmqctl -n rabbit_c@`hostname` reset
+rabbitmqctl -n rabbit_c@`hostname` join_cluster rabbit@`hostname`
+rabbitmqctl -n rabbit_c@`hostname` start_app
 
-RABBITMQ_NODE_PORT=5804 RABBITMQ_NODENAME=rabbit_4 rabbitmq-server -detached
-rabbitmqctl -n rabbit_4@`hostname` stop_app
-rabbitmqctl -n rabbit_4@`hostname` reset
-rabbitmqctl -n rabbit_4@`hostname` join_cluster rabbit_1@`hostname` --ram
-rabbitmqctl -n rabbit_4@`hostname` start_app
+RABBITMQ_NODE_PORT=5804 RABBITMQ_NODENAME=rabbit_d rabbitmq-server -detached
+rabbitmqctl -n rabbit_d@`hostname` stop_app
+rabbitmqctl -n rabbit_d@`hostname` reset
+rabbitmqctl -n rabbit_d@`hostname` join_cluster rabbit@`hostname` --ram
+rabbitmqctl -n rabbit_d@`hostname` start_app
 
-rabbitmqctl -n rabbit_1 add_user cluster_user clusterme
-rabbitmqctl -n rabbit_1 set_permissions cluster_user ".*" ".*" ".*"
+rabbitmqctl -n rabbit add_user cluster_user clusterme
+rabbitmqctl -n rabbit set_permissions cluster_user ".*" ".*" ".*"
